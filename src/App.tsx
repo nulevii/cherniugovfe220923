28ac983abfb1893ef8ae641eec42a4908285db41
Cherniugov Yevgenii <cherniugov@gmail.com>
2022-09-23 21:34:35 +0300

    change graph direction

--- src/App.tsx
+++ src/App.tsx
@@ -21,23 +21,25 @@ const Bar = ({ x, y, height, width }) => {
 
 const BarChart = ({}) =>{
   const barWidth = 20
-  const barMargin = 5
-  const width = data.length* (barWidth + barMargin)
+  const barMargin = 0
+  const height = data.length* (barWidth + barMargin)
 const greatestValue = (values) =>
   values.reduce((acc, cur) => (cur > acc ? cur : acc), -Infinity);
-const height = greatestValue(data.map((datum) => datum.time));
+const width = greatestValue(data.map((datum) => datum.time));
 
-return(
+return (
   <Chart height={height} width={width}>
-    {data.map((datum, index) =>(
-      <Bar key={datum.name}
-      x={index * (barWidth + barMargin)}
-      y={0}
-      width={barWidth}
-      height={datum.time}/>
+    {data.map((datum, index) => (
+      <Bar
+        key={datum.name}
+        y={index * (barWidth + barMargin)}
+        x={0}
+        width={datum.time}
+        height={barWidth}
+      />
     ))}
   </Chart>
-)
+);
  }
 const App: React.FC = () => {
   // const [chartValues, setChartvalues] = useState([])
