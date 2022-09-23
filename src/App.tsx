aafd0140670e9ca488aafcf0fbf7c9dfde29e29d
Cherniugov Yevgenii <cherniugov@gmail.com>
2022-09-24 00:25:11 +0300

    Finish with flexible chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable */
 // @ts-nocheck
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { data } from "./data";
 import { DataInterface } from './data-interface';
@@ -14,35 +14,95 @@ const Chart = ({ children, height, width }) => {
   );
 };
 
-const Bar = ({ x, y, height, width }) => {
-  return <rect x={x} y={y} height={height} width={width} />;
+const Bar = ({ x, y, height, width, data, fullWidth }) => {
+  console.log(fullWidth, width, x)
+  return (
+    <>
+      <g className="bar-group">
+        <text
+          className="name-label"
+          x="100"
+          y={y + height/2+5}
+          alignmentBaseline="middle"
+        >
+          {data.name}
+        </text>
+        <rect
+          className="side-bar"
+          x={120}
+          y={y}
+          height={height}
+          width={x - 120}
+        />
+        ;
+        <rect className="main-bar" x={x} y={y} height={height} width={width} />;
+        <rect
+          className="side-bar"
+          x={x+ width}
+          y={y}
+          height={height}
+          width={fullWidth - width - x}
+        />
+        ;
+        <text
+          width={width}
+          height={height}
+          className="value-label"
+          x={x + width / 2}
+          y={y + height / 2 + 4}
+          color="black"
+        >
+          {data.time}
+        </text>
+      </g>
+    </>
+  );
+
 };
 
 
 const BarChart = ({}) =>{
+  const [windowWidth, setWindowWidth] = useState(0)
+
+  const resizeChart = () => {
+          if (window.innerWidth > 2880) {
+            setWindowWidth( 2880 - 20);
+          }
+          setWindowWidth(window.innerWidth - 20);
+  }
+
+  useEffect(() => {
+    setWindowWidth(resizeChart());
+        window.addEventListener(
+          "resize",resizeChart
+
+        );
+  },[])
   const barHeight = 30
-  const barMargin = 0
+  const barMargin = 15
   const height = data.length* (barHeight + barMargin)
   const totalTime = data.reduce((acc, element) => (acc+=element.time), 0);
-  const width = window.innerWidth;
 
-        let preBarWidth = 0;
+
+        let previousBarWidth = 0;
 return (
-  <Chart height={height} width={width}>
+  <Chart height={height} width={windowWidth}>
     {data.map((datum, index) => {
 
       if (index > 0){
 
-      preBarWidth += width / totalTime * data[index - 1].time;
+      previousBarWidth += ((windowWidth - 120) / totalTime) * data[index - 1].time;
       }
       return (
         <>
           <Bar
             key={datum.name}
             y={index * (barHeight + barMargin)}
-            x={preBarWidth}
-            width={(width / totalTime) * datum.time}
+            x={previousBarWidth+ 120}
+            width={((windowWidth-120) / totalTime) * datum.time}
             height={barHeight}
+            data={datum}
+            fullWidth={windowWidth}
           />
         </>
       );})}
@@ -50,12 +110,11 @@ return (
 );
  }
 const App: React.FC = () => {
-  // const [chartValues, setChartvalues] = useState([])
   return (
-    <>
-      <h1>SPENT TIME (SECONDS)</h1>
+      <section className='chart-section'>
+      <h1 className='chart-sign'>SPENT TIME (SECONDS)</h1>
       <BarChart />
-    </>
+      </section>
   );
 };
 
